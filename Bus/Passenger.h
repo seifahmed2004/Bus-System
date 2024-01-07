b0383a4587a8e9ffab122abedfc4a7500f9e7011
Seif Ahmed <seifahmedafifi@gmail.com>
2024-01-07 04:12:10 +0200

    Cleaner code in passengers class

--- Bus/Passenger.h
+++ Bus/Passenger.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <string>
 using namespace std;
-class Passenger
-{
+
+
+class Passenger {
 private:
     string type;
     int id;
@@ -10,89 +11,33 @@ private:
     int startStation;
     int endStation;
     int priority;
-    int gettingTime;
-    int movingTime;
-    int finishTime;
+    //we initialize those variables to -1 for many reasons
+    //(1) easy detection
+    //(2) uniqueness ass not any variable is initialized to -1 in our project as it is not a valid input
+    int gettingTime = -1;
+    int movingTime = -1;
+    int finishTime = -1;
+
 public:
     Passenger(string type, int id, int arrivalTime, int startStation, int endStation, int priority)
-        : type(type), arrivalTime(arrivalTime), startStation(startStation), endStation(endStation), priority(priority), id(id)
-    {
-        gettingTime = movingTime = finishTime = -1;
-    }
-    /***GETTERS***/
-    string getType()
-    {
-        return type;
-    }
+        : type(move(type)), arrivalTime(arrivalTime), startStation(startStation), endStation(endStation), priority(priority), id(id) {}
 
-    int getId()
-    {
-        return id;
-    }
+    // Getters
+    string getType() const { return type; }
+    int getId() const { return id; }
+    int getArrivalTime() const { return arrivalTime; }
+    int getStartStation() const { return startStation; }
+    int getEndStation() const { return endStation; }
+    int getPriority() const { return priority; }
+    int getGettingTime() const { return gettingTime; }
+    int getMovingTime() const { return movingTime; }
+    int getWaitingTime() const { return movingTime - arrivalTime; }
+    int getTripTime() const { return finishTime - movingTime; }
+    int getFinishTime() const { return finishTime; }
 
-    int getArrivalTime()
-    {
-        return arrivalTime;
-    }
-
-    int getStartStation()
-    {
-        return startStation;
-    }
-
-    int getEndStation()
-    {
-        return endStation;
-    }
-
-    int getPriority()
-    {
-        return priority;
-    }
-
-    int getGettingTime()
-    {
-        return gettingTime;
-    }
-
-    int getMovingTime()
-    {
-        return movingTime;
-    }
-
-    int getWaitingTime()
-    {
-        return movingTime - arrivalTime;
-    }
-
-    int getTripTime()
-    {
-        return finishTime - movingTime;
-    }
-
-    int getFinishTime()
-    {
-        return finishTime;
-    }
-    /***SETTERS***/
-    void setMovingTime(int movingTime)
-    {
-        this->movingTime = movingTime;
-    }
-
-    void setFinishTime(int finishTime)
-    {
-        this->finishTime = finishTime;
-    }
-
-    void setGettingTime(int gettingTime)
-    {
-        this->gettingTime = gettingTime;
-    }
-
-    void setPriority(int priority) {
-        this->priority = priority;
-    }
+    // Setters
+    void setMovingTime(int movingTime) { this->movingTime = movingTime; }
+    void setFinishTime(int finishTime) { this->finishTime = finishTime; }
+    void setGettingTime(int gettingTime) { this->gettingTime = gettingTime; }
+    void setPriority(int priority) { this->priority = priority; }
 };
-
-
